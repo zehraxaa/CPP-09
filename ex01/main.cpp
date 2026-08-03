0edbfa8810a0fd596c4dea65bea1356acf57e091
zehraxaa <aysezehraaydogdu@gmail.com>
2026-08-03 13:06:55 +0300

    Create an implementation plan for Reverse Polish Notation (ex01)

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -10,3 +10,16 @@ int main(int argc, char **argv)
 		return 1;
 	}
 }
+// önce string şekilde girdiğimiz girdiği tek tek parse etsin
+// sonra her elemanı alsın, sayıysa integera çevirsin; değilse operatörlerimizden biri mi diye kontrol etsin
+// parse işlemini tek tek yapıp containera atsın
+// sonra döngü içerisinde ilk operatör gördüğü yerde [indx - 2] [op] [indx - 1] işlemini yapsın
+// ve bu sonucu bir değerde saklasın (result). en son result değerini döndürsün
+
+//hata verilecek durumlar:
+// eksik argüman -> çıkış (Error: wrong number of arguments)
+// yanlış operatör -> çıkış (Error)
+// verilen sayılar 10 dan küçük olmalı, büyükse -> çıkış (Error)
+// Error diye bir exception tanımla en iyisi
+
+// all errors will display in stdandart error
